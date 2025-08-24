1331ec45e982dce5a97e87d01ae4e5da68396cd7
mszopa <marcinsz.pv@gmail.com>
2025-08-24 16:51:12 +0200

    refactor: simplify console logging configuration

--- src/OweMe.Identity.Server/Program.cs
+++ src/OweMe.Identity.Server/Program.cs
@@ -12,7 +12,7 @@ try
     var builder = WebApplication.CreateBuilder(args);
 
     builder.Host.UseSerilog((ctx, lc) => lc
-        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+        .WriteTo.Console()
         .Enrich.FromLogContext()
         .ReadFrom.Configuration(ctx.Configuration));
     
