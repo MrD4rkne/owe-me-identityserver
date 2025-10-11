c541631def54edb3803f284e90501def99cfc993
Marcin Szopa <marcinsz.pv@gmail.com>
2025-10-11 15:23:39 +0200

    tests(wip): introduce extensions for webhostbuilder

---
+++ src/OweMe.Identity.IntegrationTests/Helpers/WebHostBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OweMe.Identity.IntegrationTests.Helpers;
+
+internal static class WebHostBuilderExtensions
+{
+    public static IWebHostBuilder WithConfigures<T>(this IWebHostBuilder builder, Action<T> configure)
+    where T : class
+    {
+        return builder.ConfigureServices(services =>
+        {
+            services.Configure(configure);
+        });
+    }
+
+    public static IWebHostBuilder WithConnectionString(this IWebHostBuilder builder, string connectionString)
+    {
+        return builder.ConfigureAppConfiguration((_, config) =>
+                       {
+                           config.AddInMemoryCollection(new Dictionary<string, string?>
+                           {
+                               ["ConnectionStrings:DefaultConnection"] = connectionString
+                           });
+                       });
+    }
+
+    public static IWebHostBuilder WithSetting(this IWebHostBuilder builder, string key, string value)
+    {
+        
+    }
+}
