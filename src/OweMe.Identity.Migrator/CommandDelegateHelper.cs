85faac9daae0d517b676c23ac758d1655325958d
Marcin Szopa <marcinsz.pv@gmail.com>
2026-07-09 20:29:34 +0200

    refactor: remove unused connection string argument and related method from CommandDelegateHelper

--- src/OweMe.Identity.Migrator/CommandDelegateHelper.cs
+++ src/OweMe.Identity.Migrator/CommandDelegateHelper.cs
@@ -9,12 +9,6 @@ namespace OweMe.Identity.Migrator;
 
 internal static class CommandDelegateHelper
 {
-    private static readonly Argument<string?> ConnectionStringArgument = new("connectionString")
-    {
-        Description = $"The connection string to the database.",
-        Arity = ArgumentArity.ZeroOrOne
-    };
-
     private static readonly Option<bool> VerboseOption = new("--verbose", "-v")
     {
         Description = "Enables verbose logging for the migration process.",
@@ -25,13 +19,11 @@ internal static class CommandDelegateHelper
     {
         rootCommand.BindCommand<MigrateCommand>(new("migrate", "Applies all pending migrations to the database.")
         {
-            Arguments = { ConnectionStringArgument },
             Options = { VerboseOption }
         });
 
         rootCommand.BindCommand<SeedCommand>(new("seed", "Seeds the database.")
         {
-            Arguments = { ConnectionStringArgument },
             Options = { VerboseOption }
         });
     }
@@ -50,7 +42,6 @@ internal static class CommandDelegateHelper
             .AddEnvironmentVariables()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json", optional: true)
-            .AddConnectionStringFromCli(parseResult)
             .Build();
 
         bool isVerbose = parseResult.GetValue(VerboseOption);
@@ -71,17 +62,5 @@ internal static class CommandDelegateHelper
             Environment.ExitCode = 1;
             return;
         }
-    }
-
-    private static IConfigurationBuilder AddConnectionStringFromCli(this IConfigurationBuilder builder,
-        ParseResult parseResult)
-    {
-        var connectionString = parseResult.GetValue(ConnectionStringArgument);
-        if (!string.IsNullOrEmpty(connectionString))
-        {
-            builder.AddInMemoryCollection([new("ConnectionStrings::DefaultConnection", connectionString)]);
-        }
-
-        return builder;
     }
 }
